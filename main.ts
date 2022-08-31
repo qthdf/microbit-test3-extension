ca5b93aee39b7070dfeed3bd5315a51fd054b7fa
qthdf <110314206+qthdf@users.noreply.github.com>
2022-08-31 14:01:12 +0800

    Update main.ts

--- main.ts
+++ main.ts
@@ -98,64 +98,67 @@ namespace Microbit {
 
 
 
-    //% weight=30 
-    //% blockId="gatorParticle_begin" 
-    //% block="initialize gator:Particle sensor"
-    //% shim=gatorParticle::begin
-    //% subcategory=传感器
-    export function begin() {
-        return
-    }
+    // Functions for reading Particle from the gatorParticle in Particle or straight adv value
+	
+	/**
+	* Initializes the gator:particle sensor, must be called on power up
+	*/	
+	//% weight=30 
+	//% blockId="gatorParticle_begin" 
+	//% block="initialize gator:Particle sensor"
+	//% shim=gatorParticle::begin
+	export function begin(){
+		return
+	}
+		
+	/**
+	* Reads either the Red or Infrared detection channels
+	*/
+	//% weight=29 
+	//% blockId="gatorParticle_color" 
+	//% block="get %LEDToRead value"
+	//% shim=gatorParticle::color
+	export function color(type: LEDToRead): number{
+		return 0
+	}
+	
+	/**
+	* Set which LED's we want the sensor to update and read.
+	*/	
+	//% weight=28
+	//% blockId="gatorParticle_setReadMode"
+	//% block="set LED mode to read %LEDMode"
+	//% shim=gatorParticle::setReadMode
+	//% advanced=true
+	export function setReadMode(mode: LEDMode)
+	{
+		return
+	}
 
-    /**
-    * Reads either the Red or Infrared detection channels
-    */
-    //% weight=29 
-    //% blockId="gatorParticle_color" 
-    //% block="get %LEDToRead value"
-    //% shim=gatorParticle::color
-    //% subcategory=传感器
-    export function color(type: LEDToRead): number {
-        return 0;
-    }
-
-    /**
-    * Set which LED's we want the sensor to update and read.
-    */
-    //% weight=28
-    //% blockId="gatorParticle_setReadMode"
-    //% block="set LED mode to read %LEDMode"
-    //% shim=gatorParticle::setReadMode
-    //% advanced=true
-    //% subcategory=传感器
-    export function setReadMode(mode: LEDMode) {
-        return;
-    }
-
-    /**
-    * Set the amplitude of either Red or Infrared LED
-    */
-    //% weight=27
-    //% blockId="gatorParticle_setAmplitude"
-    //% block="change strength of %LEDToRead | to %myBrightness"
-    //% shim=gatorParticle::setAmplitude
-    //% advanced=true
-    //% subcategory=传感器
-    export function setAmplitude(led: LEDToRead, myBrightness: number) {
-        return;
-    }
-
-    /**
-    * Grab the heartbeat from the sensor in either beats per minute, or an average of the last 4 BPM readings.
-    */
-    //% weight=26
-    //% blockId="gatorParticle_heartbeat"
-    //% block="detect heartbeat in %HeartbeatType"
-    //% shim=gatorParticle::heartbeat
-    //% subcategory=传感器
-    export function heartbeat(type: HeartbeatType): number {
-        return 0;
-    }
+	/**
+	* Set the amplitude of either Red or Infrared LED
+	*/	
+	//% weight=27
+	//% blockId="gatorParticle_setAmplitude"
+	//% block="change strength of %LEDToRead | to %myBrightness"
+	//% shim=gatorParticle::setAmplitude
+	//% advanced=true
+	export function setAmplitude(led: LEDToRead, myBrightness: number)
+	{
+		return
+	}
+	
+	/**
+	* Grab the heartbeat from the sensor in either beats per minute, or an average of the last 4 BPM readings.
+	*/
+	//% weight=26
+	//% blockId="gatorParticle_heartbeat"
+	//% block="detect heartbeat in %HeartbeatType"
+	//% shim=gatorParticle::heartbeat
+	export function heartbeat(type: HeartbeatType): number
+	{
+		return 0
+	}
 
 
 
